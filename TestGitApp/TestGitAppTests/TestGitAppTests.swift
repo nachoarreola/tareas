cd8c36fef51caa42c9559a90a6ebb53a210930da
macbookAir <cristian@encalient.es>
2018-02-06 20:27:30 -0600

    Agregando proyecto.

---
+++ TestGitApp/TestGitAppTests/TestGitAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestGitAppTests.swift
+//  TestGitAppTests
+//
+//  Created by Nacho Arreola on 2/6/18.
+//  Copyright © 2018 Nacho Arreola. All rights reserved.
+//
+
+import XCTest
+@testable import TestGitApp
+
+class TestGitAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
